2e2fe3f962561cc0ed828920117e220fc8aae5eb
eryckpedro <eryckpedro@yahoo.com.br>
2018-03-17 14:50:04 -0300

    NLCode scrollbar e monitor/gráfico GUI

--- resources/codeHandler.js
+++ resources/codeHandler.js
@@ -96,7 +96,7 @@ function sendCodeToNL()
 {
     var nlCode = generateNLCode();
 
-    var template = fs.readFileSync('./resources/template.txt', 'utf8');
+    var template = fs.readFileSync('./resources/wolf_sheep_template.txt', 'utf8');
     var modelo_dir = './modelo.nlogo';
 
     var modelo_code = nlCode + template; //Assembling user generated code + the NetLogo builtin template
